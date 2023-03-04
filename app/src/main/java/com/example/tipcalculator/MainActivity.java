6e2009c41f0577a795e249bcde5f93db92f47ff7
sushenman <sushenman83@gmail.com>
2023-03-04 16:50:28 +0545

    Tip Calculator

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -12,43 +12,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
-    private Button calculatorButton;
-    private RadioGroup radioGc;
-    private Switch roundUpSwitch;
-    private TextView tipText;
-    private double tipAmount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.costOfServiceText);
-        calculatorButton = findViewById(R.id.calculatorButton);
-        radioGc = findViewById(R.id.radioGroup);
-        roundUpSwitch = findViewById(R.id.roundUpSwitch);
-        tipText = findViewById(R.id.tipAmountText);
-
-        calculatorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-double costOfService = Double.parseDouble(editText.getText().toString());
-                int selectedId = radioGc.getCheckedRadioButtonId();
-                if (selectedId == R.id.amazingRadioButton) {
-                    tipAmount = costOfService * 0.15;
-                } else if (selectedId == R.id.goodRadioButton) {
-                    tipAmount = costOfService * 0.18;
-                } else if (selectedId == R.id.okayRadioButton) {
-                    tipAmount = costOfService * 0.20;
-                }
-
-                if (roundUpSwitch.isChecked()) {
-                    tipAmount = Math.round(tipAmount);
-                }
-
-                tipText.setText("Tip Amount: $" + tipAmount);
-            }
-        });
 
 
     }
